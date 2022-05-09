479b4e6cd4f39fd1b94db2756fdc6818b388b9da
Gabriel Inacio Wenchenck de Carvalho <wenck13@gmail.com>
2022-05-09 15:00:42 -0300

    Exercícios sobre variáveis

---
+++ modulo1/variaveis/exercicioEscrita.js
@@ -0,0 +1,45 @@
+/* Exercício 1.
+const nome = "";
+const idade = "";
+
+console.log(typeof nome);
+console.log(typeof idade); 
+*/
+
+/* primeiro, tentei sem o "=", deu erro por falta de declaração, depois tentei um parêntese vazio "()" e deu erro de sintaxe, por fim, tentei aspas vazias e foi retornado string, pois aspas é a forma de declarar uma variável do tipo string. */
+
+// const nome = prompt("Digite seu nome");
+// const idade = prompt("Digite sua idade");
+
+// console.log(typeof nome, typeof idade);
+
+// Ambas as variáveis continuam sendo strings
+
+// const nome = prompt("Digite seu nome");
+// const idade = prompt("Digite sua idade");
+// console.log("Olá", nome, "você tem", idade, "anos.");
+
+// Exercício 2.
+
+/*let pergunta1 = prompt("Você está estudando?");
+let pergunta2 = prompt("Hoje está calor?");
+let pergunta3 = prompt("Você está com sede?");
+
+console.log("Você está estudando?", pergunta1);
+console.log("Hoje está calor?", pergunta2);
+console.log("Você está com sede?", pergunta3);*/
+
+// Exercício 3
+
+/*
+let a = 10;
+let b = 25;
+let x;
+
+x = a;
+a = b;
+b = x;
+
+console.log("O novo valor de a é", a);
+console.log("O novo valor de b é", b);
+*/
