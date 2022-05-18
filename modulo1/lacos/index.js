65e779c704554448f96d85733586c46195cbde82
Gabriel Inacio Wenchenck de Carvalho <wenck13@gmail.com>
2022-05-18 16:10:07 -0300

    Execícios sobre laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,82 @@
+// Exercícios de interpretação de código
+
+// 1. O código está fazendo um contador para somar todos os números de 0 a 4
+
+// 2.
+// a. 19 21 23 25 27 30
+// b. Sim, poderia ser feito utilizando for (let numero of lista) {
+//  console.log(numero);}
+
+// 3.
+// *
+// **
+// ***
+// ****
+
+// Exercícios de escrita de código
+
+// 1.
+
+// let numeroDePets = Number(prompt("Quantos pets você tem?"));
+
+// let arrayPets = [];
+
+// for (let i = 0; i < numeroDePets; i++) {
+//   if (numeroDePets === 0) {
+//     console.log("Que pena! Você pode adotar um pet!");
+//   } else {
+//     let nomeDosPets = prompt("Qual o nome dos pets?");
+//     arrayPets.push(nomeDosPets);
+//     console.log(arrayPets);
+//   }
+// }
+
+// 2.
+
+// a.
+// const arrayOriginal = [1, 3, 7, 10, 13];
+// for (let numeros of arrayOriginal) {
+//   console.log(numeros);
+// }
+
+// b.
+// const arrayOriginal = [1, 3, 7, 10, 13];
+// for (let numeros of arrayOriginal) {
+//   numDiv10 = numeros / 10;
+//   console.log(numvDiv10);
+// }
+
+// c.
+// const arrayOriginal = [1, 2, 3, 4, 7, 8, 10, 13];
+// arrayPares = [];
+// for (let numeros of arrayOriginal) {
+//   if (numeros % 2 === 0) {
+//     arrayPares.push(numeros);
+//   }
+// }
+// console.log(arrayPares);
+
+// d.
+// const arrayOriginal = [1, 2, 3, 4, 7, 8, 10, 13];
+// novoArray = [];
+// for (let i = 0; i < arrayOriginal.length; i++) {
+//   novoArray.push(`O elemento do index ${i} é ${arrayOriginal[i]}`);
+// }
+// console.log(novoArray);
+
+// e.
+// const arrayOriginal = [2, 3, 4, 13, 8, 10, 7];
+
+// let valorMax = 0;
+// let valorMin = 99;
+// //valorMin = 14;
+// for (let i = 0; i < arrayOriginal.length; i++) {
+//   if (valorMax < arrayOriginal[i]) {
+//     valorMax = arrayOriginal[i];
+//   }
+//   if (valorMin > arrayOriginal[i]) {
+//     valorMin = arrayOriginal[i];
+//   }
+// }
+// console.log(valorMax);
+// console.log(valorMin);
