59905e3d8a5d832c8c050d084ea8a92c9902eb6e
gabrielwenchenck <wenck13@gmail.com>
2022-06-08 20:37:54 -0300

    atividade estados - insta4 - atualizado

--- modulo2/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ modulo2/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -1,34 +1,39 @@
-import React, {Component} from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const CommentContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    padding: 5px;
-`
+  display: flex;
+  justify-content: center;
+  padding: 5px;
+`;
 
 const InputComentario = styled.input`
-    width: 100%;
-    margin-right: 5px;
-`
+  width: 100%;
+  margin-right: 5px;
+`;
 
 export class SecaoComentario extends Component {
-	state = {
+  state = {
+    comentario: "",
+  };
 
-	}
+  onChangeComentario = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      comentario: event.target.value,
+    });
+  };
 
-	onChangeComentario() {
-
-	}
-
-	render() {
-		return <CommentContainer>
-			<InputComentario
-				placeholder={'Comentário'}
-				value={''}
-				onChange={this.onChangeComentario}
-			/>
-			<button onClick={this.props.aoEnviar}>Enviar</button>
-		</CommentContainer>
-	}
+  render() {
+    return (
+      <CommentContainer>
+        <InputComentario
+          type="text"
+          placeholder={"Comentário"}
+          onChange={this.onChangeComentario}
+        />
+        <button onClick={this.props.aoEnviar}>Enviar</button>
+      </CommentContainer>
+    );
+  }
 }
