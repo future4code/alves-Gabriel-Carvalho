905a16385f58d368db10ceaab076952d83a10027
gabrielwenchenck <wenck13@gmail.com>
2022-08-10 21:00:53 -0300

    typescript

---
+++ modulo4/intro-typescript/src/index.ts
@@ -0,0 +1,7 @@
+import { checaTriangulo } from "./exercicio1";
+import { imprimeTresCoresFavoritas } from "./exercicio2";
+
+console.log((checaTriangulo(2,2,2)))
+
+console.log(imprimeTresCoresFavoritas())
+
