5b0fb481ec442b6a4e8b426627999bfb2840168d
gabrielwenchenck <wenck13@gmail.com>
2022-08-11 13:57:39 -0300

    exercicios typescript

--- modulo4/intro-typescript/src/exercicio2.ts
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -1,4 +1,4 @@
-import promptSync from 'prompt-sync';
+
 export function imprimeTresCoresFavoritas():void {
    const cor1:string = prompt("Insira sua primeira cor favorita")
    const cor2:string = prompt("Insira sua segunda cor favorita")
--- modulo4/intro-typescript/src/index.ts
+++ modulo4/intro-typescript/src/index.ts
@@ -1,7 +1,16 @@
 import { checaTriangulo } from "./exercicio1";
 import { imprimeTresCoresFavoritas } from "./exercicio2";
+import {checaAnoBissexto} from "./exercicio3"
+import {comparaDoisNumeros} from "./exercicio4"
+import {checaRenovacaoRG} from "./exercicio5"
 
 console.log((checaTriangulo(2,2,2)))
 
-console.log(imprimeTresCoresFavoritas())
+//console.log(imprimeTresCoresFavoritas())
+
+console.log(checaAnoBissexto(1996))
+
+console.log(comparaDoisNumeros(10,7))
+
+console.log(checaRenovacaoRG(2022, 1996, 2014))
 
