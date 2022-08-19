5c27953211af6ed095b83bb6d748c4d9ed892da2
gabrielwenchenck <wenck13@gmail.com>
2022-08-19 17:24:29 -0300

    inicio do projeto

---
+++ modulo4/projeto-sistema-bancario/src/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+  console.log("Server is running at port 3003");
+});
