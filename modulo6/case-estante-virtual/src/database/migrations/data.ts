3e851be37350beb32608f80f7d631345cd9600d2
gabrielwenchenck <wenck13@gmail.com>
2022-10-16 23:26:56 -0300

    case-estante-virtual 5

--- modulo6/case-estante-virtual/src/database/migrations/data.ts
+++ modulo6/case-estante-virtual/src/database/migrations/data.ts
@@ -1,25 +1,102 @@
-// import { IUserDB, USER_ROLES } from "../../models/Athlete";
+import { IResultDB } from "./../../models/Result";
+import { IAthleteDB } from "../../models/Athlete";
+import { ICompetitionDB } from "../../models/Competition";
 
-// export const users: IUserDB[] = [
-//   {
-//     id: "101",
-//     name: "Astrodev",
-//     email: "astrodev@gmail.com",
-//     password: "$2a$12$RBAWOHpUvGTE.MEeIohAzec9tlVqtNA/x2PMPt/Hrt0vI437cQdJC", // bananinha
-//     role: USER_ROLES.ADMIN,
-//   },
-//   {
-//     id: "102",
-//     name: "Fulano",
-//     email: "fulano@gmail.com",
-//     password: "$2a$12$PULtVNlAll87D6E8pR/0HO9vbzVDPaUMA89rc5cNmYoAAepbwmkcO", // qwerty00
-//     role: USER_ROLES.NORMAL,
-//   },
-//   {
-//     id: "103",
-//     name: "Ciclana",
-//     email: "ciclana@gmail.com",
-//     password: "$2a$12$LkWMqS3oPhP2iVMcZOVvWer9ahUPulxjB0EA4TWPxWaRuEEfYGu/i", // asdfg123
-//     role: USER_ROLES.NORMAL,
-//   },
-// ];
+export const athletes: IAthleteDB[] = [
+  {
+    id: "101",
+    name: "Phelps",
+  },
+  {
+    id: "102",
+    name: "Cielo",
+  },
+  {
+    id: "103",
+    name: "Ledecky",
+  },
+  {
+    id: "104",
+    name: "Železný",
+  },
+  {
+    id: "105",
+    name: "Špotáková",
+  },
+  {
+    id: "106",
+    name: "Menéndez",
+  },
+];
+
+export const competitions: ICompetitionDB[] = [
+  {
+    id: "101",
+    name: "100m final",
+    attempts: 1,
+    is_closed: false,
+  },
+  {
+    id: "102",
+    name: "Dardo final",
+    attempts: 3,
+    is_closed: false,
+  },
+];
+
+export const results: IResultDB[] = [
+  {
+    id: "101",
+    value: 49.7,
+    value_2: 0,
+    value_3: 0,
+    unit: "s",
+    competition_id: "101",
+    athlete_id: "101",
+  },
+  {
+    id: "102",
+    value: 47.5,
+    value_2: 0,
+    value_3: 0,
+    unit: "s",
+    competition_id: "101",
+    athlete_id: "102",
+  },
+  {
+    id: "103",
+    value: 48.3,
+    value_2: 0,
+    value_3: 0,
+    unit: "s",
+    competition_id: "101",
+    athlete_id: "103",
+  },
+  {
+    id: "104",
+    value: 90.57,
+    value_2: 98.48,
+    value_3: 95.35,
+    unit: "m",
+    competition_id: "102",
+    athlete_id: "104",
+  },
+  {
+    id: "105",
+    value: 94.97,
+    value_2: 92.28,
+    value_3: 99.13,
+    unit: "m",
+    competition_id: "102",
+    athlete_id: "105",
+  },
+  {
+    id: "106",
+    value: 85.57,
+    value_2: 89.26,
+    value_3: 94.77,
+    unit: "m",
+    competition_id: "102",
+    athlete_id: "106",
+  },
+];
